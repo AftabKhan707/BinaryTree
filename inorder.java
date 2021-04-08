8d43fad970a73f709d0a2d9c6bc1b9be0bb464f4
AftabKhan707 <74177000+AftabKhan707@users.noreply.github.com>
2021-04-08 06:30:20 +0530

    Add files via upload
    Leetcode 94

---
+++ inorder.java
@@ -0,0 +1,45 @@
+package tree;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+ class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+ }
+
+
+public class inorder {
+
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> res = new LinkedList<Integer>();
+		
+		if (root == null)
+			return res;
+
+		Stack<TreeNode> st = new Stack<TreeNode>();
+		
+		TreeNode curr = root;
+		while (curr != null || !st.isEmpty()) {
+			
+			while (curr != null) {
+				st.push(curr);
+				curr = curr.left;
+			}
+			
+			curr = st.pop(); 
+			res.add(curr.val); 
+			curr = curr.right; 
+		}
+		return res;
+	}
+
+}
