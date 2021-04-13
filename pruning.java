f8aeb7559239ea4e33d267e6f8c8d4c415a344c1
AftabKhan707 <74177000+AftabKhan707@users.noreply.github.com>
2021-04-13 08:45:05 +0530

    Add files via upload

---
+++ pruning.java
@@ -0,0 +1,19 @@
+package tree;
+
+public class pruning {
+	
+	
+	    public TreeNode pruneTree(TreeNode root) {
+	      
+	    	if (root == null) 
+	    		return null;
+	    	
+	        root.left = pruneTree(root.left);
+	        root.right = pruneTree(root.right);
+	        if(root.val == 1 || root.left != null || root.right != null)
+	        	return root;
+	        else return null;
+	    }
+	
+
+}
